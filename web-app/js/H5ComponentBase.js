15ff5b3dbf35238fd19898ae639ae5df1b35fbf6
liuxinlei <741447430@qq.com>
2016-07-07 10:31:28 +0800

    ComponentBase

--- web-app/js/H5ComponentBase.js
+++ web-app/js/H5ComponentBase.js
@@ -1,2 +1,12 @@
 /* 基本图文组件对象 */
+var H5ComponentBase = function(ctg) {
+	"use strict";
+	var id = ('h5_component_base_' + Math.random()).replace('.', '_');
+	var $component = $('<div class="h5_component_base"></div>');
 
+	ctg.text && $component.text(ctg.text);
+	ctg.width && $component.width(ctg.width / 2);
+	ctg.height && $component.height(ctg.height / 2);
+
+	return $component;
+};
