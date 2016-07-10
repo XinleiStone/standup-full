5f24e05cef424b70f039a5b7aeddc1e6ebf26897
XinleiStone <741447430@qq.com>
2016-07-10 18:52:30 +0800

    component

--- web-app/js/H5ComponentPoint.js
+++ web-app/js/H5ComponentPoint.js
@@ -1,2 +1,13 @@
 /* 散点图表组件对象 */
+var H5ComponentPoint = function(name, cfg) {
+    "use strict";
+    
+    var $component = new H5ComponentBase(name, cfg);
+    var base = cfg.data[0][1];
 
+    $.each(cfg.data, function(index, el) {
+        console.log(el);
+    });     
+
+    return $component;
+};
